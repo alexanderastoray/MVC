1990a3d8ec8a215cf81297a9616fc148f1540724
alexanderastoray <47002689+alexanderastoray@users.noreply.github.com>
2020-06-23 15:36:51 +0200

    Update git

--- SolutionResto/ChoixResto/ViewModels/RestaurantCheckBoxViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ChoixResto.ViewModels
-{
-    public class RestaurantCheckBoxViewModel
-    {
-        public int Id { get; set; }
-        public string NomEtTelephone { get; set; }
-        public bool EstSelectionne { get; set; }
-    }
-}
